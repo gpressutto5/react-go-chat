fba0ea301b8e103e3861bada56fa426eda1ce6e0
Guilherme Pressutto <gpressutto5@gmail.com>
2018-03-16 19:45:01 -0300

    Added messages section

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import ChannelSection from './channels/ChannelSection.jsx'
 import UserSection from './users/UserSection.jsx'
+import MessageSection from './messages/MessageSection.jsx'
 
 class App extends Component {
   constructor(props){
@@ -9,6 +10,7 @@ class App extends Component {
       channels: [],
       activeChannel: {},
       users: [],
+      messages: [],
     }
   }
   addChannel(name){
@@ -33,6 +35,19 @@ class App extends Component {
     this.setState({users})
     // TODO: Send to server
   }
+  addMessage(body){
+    let {messages, users} = this.state
+    let createdAt = new Date
+    let author = users.length > 0 ? users[0].name : 'anonymous'
+    messages.push({
+      id: messages.length,
+      body,
+      createdAt,
+      author,
+    })
+    this.setState({messages})
+    // TODO: Send to server
+  }
   render(){
     return (
       <div className='app'>
@@ -47,6 +62,10 @@ class App extends Component {
             setUserName={this.setUserName.bind(this)}
           />
         </div>
+        <MessageSection
+          {...this.state}
+          addMessage={this.addMessage.bind(this)}
+        />
       </div>
     )
   }
---
+++ client/components/messages/MessageForm.jsx
@@ -0,0 +1,35 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+
+class MessageForm extends Component {
+  onSubmit(e){
+    e.preventDefault()
+    const node = this.refs.message
+    const message = node.value
+    this.props.addMessage(message)
+    node.value = ''
+  }
+  render(){
+    console.log(this.props.activeChannel)
+    return (
+      <form onSubmit={this.onSubmit.bind(this)}>
+        <div className='form-group'>
+          <input
+            className='form-control'
+            placeholder='Add message'
+            type='text'
+            ref='message'
+            disabled={!this.props.activeChannel.hasOwnProperty('id')}
+          />
+        </div>
+      </form>
+    )
+  }
+}
+
+MessageForm.propTypes = {
+  activeChannel: PropTypes.object.isRequired,
+  addMessage: PropTypes.func.isRequired,
+}
+
+export default MessageForm
