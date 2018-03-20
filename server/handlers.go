82801bfbf720d737098280d86753bcc6170c1985
Guilherme Pressutto <gpressutto5@gmail.com>
2018-03-20 14:08:38 -0300

    Using rethinkdb for channels

--- server/handlers.go
+++ server/handlers.go
@@ -1,21 +1,69 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/mitchellh/mapstructure"
+	r "gopkg.in/gorethink/gorethink.v4"
+)
+
+const (
+	ChannelStop = iota
+	UserStop
+	MessageStop
 )
 
 func addChannel(client *Client, data interface{}) {
 	var channel Channel
-	var message Message
 
-	mapstructure.Decode(data, &channel)
-	fmt.Printf("%#v", channel)
+	err := mapstructure.Decode(data, &channel)
+	if err != nil {
+		client.send <- Message{"error", err.Error()}
+		return
+	}
 
-	channel.ID = "123"
-	message.Name = "channel add"
-	message.Data = channel
+	go func() {
+		err := r.Table("channel").
+			Insert(channel).
+			Exec(client.session)
+		if err != nil {
+			client.send <- Message{"error", err.Error()}
+		}
+	}()
+}
 
-	client.send <- message
+func subscribeChannel(client *Client, data interface{}) {
+	stop := client.NewStopChannel(ChannelStop)
+	result := make(chan r.ChangeResponse)
+
+	cursor, err := r.Table("channel").
+		Changes(r.ChangesOpts{IncludeInitial: true}).
+		Run(client.session)
+	if err != nil {
+		client.send <- Message{"error", err.Error()}
+		return
+	}
+
+	go func() {
+		var change r.ChangeResponse
+		for cursor.Next(&change) {
+			result <- change
+		}
+	}()
+
+	go func() {
+		for {
+			select {
+			case <-stop:
+				cursor.Close()
+				return
+			case change := <-result:
+				if change.NewValue != nil && change.NewValue == nil {
+					client.send <- Message{"channel add", change.NewValue}
+				}
+			}
+		}
+	}()
+}
+
+func unsubscribeChannel(client *Client, data interface{}) {
+	client.StopForKey(ChannelStop)
 }
--- server/main.go
+++ server/main.go
@@ -1,18 +1,36 @@
 package main
 
 import (
+	"log"
 	"net/http"
+
+	r "gopkg.in/gorethink/gorethink.v4"
 )
 
 type Channel struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
+	ID   string `json:"id" gorethink:"id,omitempty"`
+	Name string `json:"name" gorethink:"name"`
+}
+
+type User struct {
+	ID   string `json:"id" gorethink:"id,omitempty"`
+	Name string `json:"name" gorethink:"name"`
 }
 
 func main() {
-	router := NewRouter()
+	session, err := r.Connect(r.ConnectOpts{
+		Address:  "localhost:28015",
+		Database: "chat",
+	})
+	if err != nil {
+		log.Panic(err.Error())
+	}
+
+	router := NewRouter(session)
 
 	router.Handle("channel add", addChannel)
+	router.Handle("channel subscribe", subscribeChannel)
+	router.Handle("channel unsubscribe", unsubscribeChannel)
 
 	http.Handle("/", router)
 	http.ListenAndServe(":4000", nil)
